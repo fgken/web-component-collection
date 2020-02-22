c6ba7f868ecdccfdffcf84563913f5981b97ed29
fgken <fg.bell.tree.333@gmail.com>
2020-02-23 00:27:44 +0900

    time series line chart

--- src/components/recharts/TimeSeriesLineChart.tsx
+++ src/components/recharts/TimeSeriesLineChart.tsx
@@ -4,42 +4,19 @@ import React, { PureComponent } from 'react'
 import {
   LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, ReferenceArea
 } from 'recharts'
+import moment from "moment"
 
 interface TimeSeriesData {
   [key: string]: number;
 }
-const data: Array<TimeSeriesData> = [
-  { name: 1, cost: 4.11, impression: 100 },
-  { name: 2, cost: 2.39, impression: 120 },
-  { name: 3, cost: 1.37, impression: 150 },
-  { name: 4, cost: 1.16, impression: 180 },
-  { name: 5, cost: 2.29, impression: 200 },
-  { name: 6, cost: 3, impression: 499 },
-  { name: 7, cost: 0.53, impression: 50 },
-  { name: 8, cost: 2.52, impression: 100 },
-  { name: 9, cost: 1.79, impression: 200 },
-  { name: 10, cost: 2.94, impression: 222 },
-  { name: 11, cost: 4.3, impression: 210 },
-  { name: 12, cost: 4.41, impression: 300 },
-  { name: 13, cost: 2.1, impression: 50 },
-  { name: 14, cost: 8, impression: 190 },
-  { name: 15, cost: 0, impression: 300 },
-  { name: 16, cost: 9, impression: 400 },
-  { name: 17, cost: 3, impression: 200 },
-  { name: 18, cost: 2, impression: 50 },
-  { name: 19, cost: 3, impression: 100 },
-  { name: 20, cost: 7, impression: 100 },
-]
+const data: Array<TimeSeriesData> = []
 
-const getAxisYDomain = (from: number, to: number, ref: string, offset: number) => {
-  const refData = data.slice(from - 1, to)
-  let [bottom, top]: Array<number> = [refData[0][ref], refData[0][ref]]
-  refData.forEach((d) => {
-    if (d[ref] > top) top = d[ref]
-    if (d[ref] < bottom) bottom = d[ref]
-  })
-
-  return [(bottom | 0) - offset, (top | 0) + offset]
+function getRandomInt(max: number) {
+  return Math.floor(Math.random() * Math.floor(max))
+}
+data.push({ time: 0, cost: getRandomInt(1000), impression: getRandomInt(1000) })
+for (let i = 1; i < 30; i++) {
+  data.push({ time: 1000 * 60 * 60 * i, cost: getRandomInt(1000), impression: getRandomInt(1000) })
 }
 
 interface TimeSeriesLineChartState {
@@ -48,10 +25,6 @@ interface TimeSeriesLineChartState {
   right: string | number;
   refAreaLeft: string | number;
   refAreaRight: string | number;
-  top: string | number;
-  bottom: string | number;
-  top2: string | number;
-  bottom2: string | number;
 }
 
 const initialState: TimeSeriesLineChartState = {
@@ -60,10 +33,6 @@ const initialState: TimeSeriesLineChartState = {
   right: 'dataMax',
   refAreaLeft: '',
   refAreaRight: '',
-  top: 'dataMax+1',
-  bottom: 'dataMin-1',
-  top2: 'dataMax+20',
-  bottom2: 'dataMin-20',
 }
 
 export default class TimeSeriesLineChart extends PureComponent<{}, TimeSeriesLineChartState> {
@@ -86,21 +55,12 @@ export default class TimeSeriesLineChart extends PureComponent<{}, TimeSeriesLin
 
     // xAxis domain
     if (refAreaLeft > refAreaRight) [refAreaLeft, refAreaRight] = [refAreaRight, refAreaLeft]
-
-    // yAxis domain
-    const [bottom, top] = getAxisYDomain(refAreaLeft as number, refAreaRight as number, 'cost', 1)
-    const [bottom2, top2] = getAxisYDomain(refAreaLeft as number, refAreaRight as number, 'impression', 50)
-
     this.setState(() => ({
       refAreaLeft: '',
       refAreaRight: '',
       data: data.slice(),
       left: refAreaLeft,
       right: refAreaRight,
-      bottom,
-      top,
-      bottom2,
-      top2,
     }))
   }
 
@@ -112,16 +72,12 @@ export default class TimeSeriesLineChart extends PureComponent<{}, TimeSeriesLin
       refAreaRight: '',
       left: 'dataMin',
       right: 'dataMax',
-      top: 'dataMax+1',
-      bottom: 'dataMin',
-      top2: 'dataMax+50',
-      bottom2: 'dataMin+50',
     }))
   }
 
   render() {
     const {
-      data, left, right, refAreaLeft, refAreaRight, top, bottom, top2, bottom2,
+      data, left, right, refAreaLeft, refAreaRight,
     } = this.state
 
     return (
@@ -144,31 +100,37 @@ export default class TimeSeriesLineChart extends PureComponent<{}, TimeSeriesLin
         >
           <CartesianGrid strokeDasharray="3 3" />
           <XAxis
-            allowDataOverflow
-            dataKey="name"
+            allowDataOverflow={true}
+            dataKey="time"
             domain={[left, right]}
             type="number"
+            tickFormatter={(unixTime) => moment(unixTime).format('HH:mm:ss')}
           />
           <YAxis
-            allowDataOverflow
-            domain={[bottom, top]}
+            allowDataOverflow={true}
             type="number"
-            yAxisId="1"
           />
-          <YAxis
-            orientation="right"
-            allowDataOverflow
-            domain={[bottom2, top2]}
-            type="number"
-            yAxisId="2"
+          <Tooltip
+            labelFormatter={(label) => moment(label).format("YYYY/MM/DD HH:mm:ss")}
           />
-          <Tooltip />
-          <Line yAxisId="1" type="natural" dataKey="cost" stroke="#8884d8" animationDuration={300} />
-          <Line yAxisId="2" type="natural" dataKey="impression" stroke="#82ca9d" animationDuration={300} />
+          <Line
+            type="monotoneX"
+            dataKey="cost"
+            stroke="#8884d8"
+            animationDuration={300}
+            connectNulls={true}
+          />
+          <Line
+            type="monotoneX"
+            dataKey="impression"
+            stroke="#8884d8"
+            animationDuration={300}
+            connectNulls={true}
+          />
 
           {
             (refAreaLeft && refAreaRight) ? (
-              <ReferenceArea yAxisId="1" x1={refAreaLeft} x2={refAreaRight} strokeOpacity={0.3} />) : null
+              <ReferenceArea x1={refAreaLeft} x2={refAreaRight} strokeOpacity={0.3} />) : null
           }
         </LineChart>
 
